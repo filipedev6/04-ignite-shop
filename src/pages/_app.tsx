b2ea63735bd8b2532cc4b0084a61cd7c2f337aef
filipedev6 <eulerfilipe8@outlook.com>
2023-08-31 16:31:24 -0300

    commit initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+import { ButtonCheckout, Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ShoppingBag } from '@phosphor-icons/react'
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Link href="/">
+          <Image src={logoImg.src} alt="" width={130} height={52} />
+        </Link>
+
+        <ButtonCheckout>
+          <ShoppingBag />
+        </ButtonCheckout>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
